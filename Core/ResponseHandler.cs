575adb401048e3cccb5afe1eb41191563012e1a8
x x <xterlo@yandex.ru>
2024-01-28 22:18:44 +0300

    add auth window

---
+++ Core/ResponseHandler.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using PasswordManager.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Core
+{
+    public  static class ResponseHandler
+    {
+        private class AuthData {
+            public string login { get;set; }
+            public string password { get;set; }
+        }
+
+        public static ResponseModel SendAuth(string login,string password)
+        {
+            
+            AuthData data = new AuthData() { login=login, password = password };
+            var client = new RestClient("https://localhost:32776/");
+            var request = new RestRequest("/api/Auth/Auth");
+            request.Method = Method.Post;
+            request.AddHeader("Content-Type", "application/json");
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(JsonConvert.SerializeObject(data));
+            var response = client.ExecutePost(request);
+            return JsonConvert.DeserializeObject<ResponseModel>(response.Content);
+            
+
+        }
+    }
+}
---
+++ Models/ResponseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Models
+{
+    public class ResponseModel
+    {
+        public bool status { get; set; }
+        public string message { get; set; }
+        public object data { get; set; }
+    }
+
+    public class AuthData
+    {
+        public string session { get; set; }
+        public string name { get; set; }
+    }
+}
