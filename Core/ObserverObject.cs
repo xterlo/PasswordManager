40be135fedd351f6e93cc6faf0a21335006f37d8
xterl <xterl@XTERLO>
2024-01-28 19:04:57 +0300

    Добавьте файлы проекта.

---
+++ Core/ObserverObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PasswordManager.Core
+{
+    internal class ObserverObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ MVVM/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PasswordManager.Core;
+
+
+namespace PasswordManager.MVVM.ViewModel
+{
+    internal class MainWindowViewModel : ObserverObject
+    {
+        private string _Title { get; set; } = "test1";
+        public string Title
+        {
+            get => _Title;
+            set { _Title = value; OnPropertyChanged(nameof(Title)); }
+        }
+    }
+}
